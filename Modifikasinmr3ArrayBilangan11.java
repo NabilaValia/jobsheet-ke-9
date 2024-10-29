52375275f42a62856563c05cfed549897a166ef8
NabilaValia <nabilaputrivaliandra29@gmail.com>
2024-10-29 11:52:11 +0700

    modifikasi array bilangan no.3

---
+++ Modifikasinmr3ArrayBilangan11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Modifikasinmr3ArrayBilangan11 {
+
+    public static void main(String[] args) {
+
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println(bil[i]);
+        }        
+    }
+}
