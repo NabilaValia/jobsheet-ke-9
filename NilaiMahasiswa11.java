e0aee4f95f981c7d71d47fd36b726a54f65f7b8f
NabilaValia <nabilaputrivaliandra29@gmail.com>
2024-10-29 13:25:58 +0700

    TUGAS NO.1

---
+++ NilaiMahasiswa11.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class NilaiMahasiswa11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah nilai mahasiswa yang akan diinput: ");
+        int jumlahNilai = scanner.nextInt();
+
+        int[] nilaiMahasiswa = new int[jumlahNilai];
+
+        for (int i = 0; i < jumlahNilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMahasiswa[i] = scanner.nextInt();
+        }
+        double totalNilai = 0;
+        for (int nilai : nilaiMahasiswa) {
+            totalNilai += nilai;
+        }
+        double rataRata = totalNilai / jumlahNilai;
+
+        int nilaiTertinggi = nilaiMahasiswa[0];
+        int nilaiTerendah = nilaiMahasiswa[0];
+        for (int nilai : nilaiMahasiswa) {
+            if (nilai > nilaiTertinggi) {
+                nilaiTertinggi = nilai;
+            }
+            if (nilai < nilaiTerendah) {
+                nilaiTerendah = nilai;
+            }
+        }
+        System.out.println("\nNilai mahasiswa yang telah dimasukkan:");
+        for (int nilai : nilaiMahasiswa) {
+            System.out.println(nilai);
+        }
+        System.out.println("\nNilai rata-rata: " + rataRata);
+        System.out.println("Nilai tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai terendah: " + nilaiTerendah);
+
+        scanner.close();
+    }
+}
