4ad9a2546e37831f9e5b6ac7c9d75c6a9b4e3453
NabilaValia <nabilaputrivaliandra29@gmail.com>
2024-10-29 12:41:51 +0700

    MODIFIKASI ARRAY RATA NILAI 11

---
+++ ModifikasiArrayRataNilai11.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ModifikasiArrayRataNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiMhs = new int[10];
+        double rata2, total = 0;
+        int jmlLulus = 0;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+            total += nilaiMhs[i];
+        }
+
+        rata2 = total / nilaiMhs.length;
+        System.out.println("Rata-rata nilai = " + rata2);
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " lulus!");
+                jmlLulus++;
+            } else {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " tidak lulus.");
+            }
+        }
+        System.out.println("Banyak mahasiswa yang lulus sebanyak: " + jmlLulus);
+        sc.close();
+    }
+}
