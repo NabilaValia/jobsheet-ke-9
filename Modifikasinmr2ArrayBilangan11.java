5f8bcdb46d478bedc5d9f06bfe9ec869076722ec
NabilaValia <nabilaputrivaliandra29@gmail.com>
2024-10-29 11:47:12 +0700

    modifikasi array bilangan no.2

--- Modifikasinmr2ArrayBilangan11.java
+++ Modifikasinmr2ArrayBilangan11.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Modifikasinmr2ArrayBilangan11 {
 
     public static void main(String[] args) {
-
         int[] bil = {5, 13, -7, 17};
 
-        for (int i = 0; i < bil.length; i++) {
-            System.out.println(bil[i]);
+        System.out.println(bil [0] );
+        System.out.println(bil [1] );
+        System.out.println(bil [2] );
+        System.out.println(bil [3] );
+
         }
     }
-}
